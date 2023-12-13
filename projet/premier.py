a536dc5722cbe061083e59de81b24a823a93f1b4
Samuel <118735723+SamuelDelabranche@users.noreply.github.com>
2023-12-13 09:43:13 +0100

    .

---
+++ projet/premier.py
@@ -0,0 +1,9 @@
+import flask
+app = flask.Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return flask.render_template("index.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
