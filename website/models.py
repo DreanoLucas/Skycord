350d6722aa2030890db97a1db5ca45f9221402c3
Chadi-Mangle <118349600+Chadi-Mangle@users.noreply.github.com>
2023-12-20 10:57:35 +0000

    Ajout dans la structure de la base de donnée

--- website/models.py
+++ website/models.py
@@ -2,18 +2,54 @@ from . import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
 
-class Message(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    data = db.Column(db.String(10000))
-    date = db.Column(db.DateTime(timezone=True), default=func.now())
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
 class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(150), unique=True, nullable=False)
-    login = db.Column(db.String(50), unique=True, nullable=False)
-    password = db.Column(db.String(100), nullable=False)
-    name = db.Column(db.String(100), nullable=False)
-    message = db.relationship('Message')
-    confirmed = db.Column(db.Boolean, default=False)
-    token = db.Column(db.String(100))
+  id = db.Column(db.Integer, primary_key=True)
+  email = db.Column(db.String(150), unique=True, nullable=False)
+  login = db.Column(db.String(50), unique=True, nullable=False)
+  password = db.Column(db.String(100), nullable=False)
+  name = db.Column(db.String(100), nullable=False)
+  message = db.relationship('Message')  # pas compris l'utilité
+  confirmed = db.Column(db.Boolean, default=False)
+  token = db.Column(db.String(100))
+
+
+class Chat(db.Model):
+  chat_id = db.Column(db.Integer, primary_key=True)
+  chat_name = db.Column(db.String(50), unique=True, nullable=False)
+  is_group = db.Column(db.Boolean, default=False)
+
+
+class ChatMember(db.Model):
+  chat_id = db.Column(db.Integer, db.ForeignKey('chat.chat_id'), primary_key=True)
+  user_id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
+
+class Message(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  date = db.Column(db.DateTime(timezone=True), default=func.now())
+  text = db.Column(db.String(1000), nullable=False)
+  user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+  chat_id = db.Column(db.Integer, db.ForeignKey('chat.chat_id'))
+
+
+""" 
+/!\ A tester : 
+
+Avec les tables actuel pour recuperer les groupes de l'utilisateur. 
+select C.*, U2.name from user U
+
+join ChatMember CM on 
+	U.id = CM.user_id
+
+join Chat C ON
+	CM.chat_id = CM.chat
+
+join Message M ON
+	M.date = max(M.Date) 
+	C.chat_id = M.chat_id 
+
+join user U2
+	M.user_id = U2.user_id
+
+Order by M.date DESC
+"""
