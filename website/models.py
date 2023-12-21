48acfbeebddb1e5d773d5be2483594b29e8130fa
Chadi-Mangle <118349600+Chadi-Mangle@users.noreply.github.com>
2023-12-21 10:52:55 +0000

    Affichages des chat de l'utilisateur (version non testé)

--- website/models.py
+++ website/models.py
@@ -9,7 +9,7 @@ class User(db.Model, UserMixin):
   login = db.Column(db.String(30), unique=True, nullable=False)
   password = db.Column(db.String(20), nullable=False)
   name = db.Column(db.String(20), nullable=False)
-  profil = db.Column(db.String(250), nullable=True)
+  profil = db.Column(db.String(250)) 
   message = db.relationship('Message')  # pas compris l'utilité
   confirmed = db.Column(db.Boolean, default=False)
   token = db.Column(db.String(100))
@@ -17,7 +17,7 @@ class User(db.Model, UserMixin):
 
 class Chat(db.Model):
   chat_id = db.Column(db.Integer, primary_key=True)
-  chat_name = db.Column(db.String(100), unique=True, nullable=False)
+  chat_name = db.Column(db.String(100))
   is_group = db.Column(db.Boolean, default=False) # a voir si c'est utile
 
 
@@ -34,23 +34,31 @@ class Message(db.Model):
 
 
 """ 
-/!\ A tester : 
+Avec les tables actuel pour recuperer les chats de l'utilisateur. 
 
-Avec les tables actuel pour recuperer les groupes de l'utilisateur. 
-select C.*, U2.name from user U
+SELECT C.chat_id, c.chat_name, M.text
+FROM user U
+JOIN chat_member CM ON CM.user_id = U.id
+JOIN chat C ON C.chat_id = CM.chat_id
 
-join ChatMember CM on 
-	U.id = CM.user_id
+JOIN (
+    SELECT chat_id, MAX(date) AS max_date
+    FROM Message
+    GROUP BY chat_id
+) MAX ON C.chat_id = MAX.chat_id
 
-join Chat C ON
-	CM.chat_id = CM.chat
+JOIN message M ON 
+M.chat_id = C.chat_id 
+AND M.date = MAX.max_date
+WHERE U.id = -- Mettre l'id de l'utilisateur ici
+ORDER By M.date DESC
 
-join Message M ON
-	M.date = max(M.Date) 
-	C.chat_id = M.chat_id 
 
-join user U2
-	M.user_id = U2.user_id
 
-Order by M.date DESC
+INSERT INto chat VALUES (1, 'test', 0)
+INSERT INTO chat_member VALUES (1, 4)
+INSERT INTO chat_member VALUES (1, 3)
+
+INSERT INTO Message (date, text, user_id, chat_id)
+VALUES (datetime('now'), 'test', 3, 1);
 """
--- website/views.py
+++ website/views.py
@@ -1,5 +1,10 @@
+#faudrait deplacer le sql dans un fichier sqlrequest.py mais deja je voudrais savoir si ça marche
+
 from flask import Blueprint, render_template, url_for, redirect
 from flask_login import login_required, current_user
+
+from . import db
+from .models import *
 
 views = Blueprint('views', __name__)
 
@@ -7,4 +12,23 @@ views = Blueprint('views', __name__)
 @views.route('/')
 @login_required
 def home():
-  return render_template('page_accueil.html', user=current_user)
+  subquery = db.session.query(Message.chat_id,
+                              func.max(
+                                  Message.date).label('max_date')).group_by(
+                                      Message.chat_id).subquery()
+
+  user_chats = db.session.query(
+      Chat.chat_id, Chat.chat_name, Message.text,
+      Message.date).join(ChatMember, ChatMember.chat_id == Chat.chat_id).join(
+          User, User.id == ChatMember.user_id).join(
+              subquery, subquery.c.chat_id == Chat.chat_id).join(
+                  Message,
+                  db.and_(Message.chat_id == Chat.chat_id,
+                          Message.date == subquery.c.max_date)).filter(
+                              User.id == current_user.id).order_by(
+                                  Message.date.desc())
+
+  print(user_chats.all())
+  return render_template('page_accueil.html',
+                         user=current_user,
+                         chats=user_chats.all())
