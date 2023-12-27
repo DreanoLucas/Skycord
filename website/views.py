f10d1e4d66dc448ef747215a6b8b95ecee85d8ca
Dreano Lucas <49568908+DreanoLucas@users.noreply.github.com>
2023-12-27 20:33:48 +0100

    requete groupe

--- website/views.py
+++ website/views.py
@@ -34,4 +34,24 @@ def home():
         Rendered template: Renders the 'acceuil.html' template with user information.
     """
 
-    return render_template('acceuil.html', user=current_user)
+    def home():
+  subquery = db.session.query(Message.chat_id,
+                              func.max(
+                                  Message.date).label('max_date')).group_by(
+                                      Message.chat_id).subquery()
+
+  user_chats = db.session.query(
+      Chat.chat_id, Chat.chat_name, Message.text,
+      Message.date).join(ChatMember, ChatMember.chat_id == Chat.chat_id).join(
+          User, User.id == ChatMember.user_id).join(
+              subquery, subquery.c.chat_id == Chat.chat_id).join(
+                  Message,
+                  db.and_(Message.chat_id == Chat.chat_id,
+                          Message.date == subquery.c.max_date)).filter(
+                              User.id == current_user.id).order_by(
+                                  Message.date.desc())
+
+  print(user_chats.all())
+  return render_template('page_accueil.html',
+                         user=current_user,
+                         chats=user_chats.all())
