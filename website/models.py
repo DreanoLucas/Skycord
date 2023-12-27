c26b0d26864aca7bf48b82b05340cd86c5ed5495
Dreano Lucas <49568908+DreanoLucas@users.noreply.github.com>
2023-12-27 20:35:42 +0100

    groupe

--- website/models.py
+++ website/models.py
@@ -21,22 +21,35 @@ from . import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
 
-class Message(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    data = db.Column(db.String(10000))
-    date = db.Column(db.DateTime(timezone=True), default=func.now())
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-
 class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(150), unique=True, nullable=False)
-    login = db.Column(db.String(50), unique=True, nullable=False)
-    password = db.Column(db.String(100), nullable=False)
-    name = db.Column(db.String(100), nullable=False)
-    message = db.relationship('Message')
-    confirmed = db.Column(db.Boolean, default=False)
-    token = db.Column(db.String(100))
-    friends = db.relationship('Friend', foreign_keys='Friend.user_id', backref='user', lazy=True)
+  id = db.Column(db.Integer, primary_key=True)
+  email = db.Column(db.String(50), unique=True, nullable=False)
+  login = db.Column(db.String(30), unique=True, nullable=False)
+  password = db.Column(db.String(20), nullable=False)
+  name = db.Column(db.String(20), nullable=False)
+  profil = db.Column(db.String(250)) 
+  message = db.relationship('Message')  # pas compris l'utilité
+  confirmed = db.Column(db.Boolean, default=False)
+  token = db.Column(db.String(100))
+  friends = db.relationship('Friend', foreign_keys='Friend.user_id', backref='user', lazy=True)
+
+
+class Chat(db.Model):
+  chat_id = db.Column(db.Integer, primary_key=True)
+  chat_name = db.Column(db.String(100))
+  is_group = db.Column(db.Boolean, default=False) # a voir si c'est utile
+
+
+class ChatMember(db.Model):
+  chat_id = db.Column(db.Integer, db.ForeignKey('chat.chat_id'), primary_key=True)
+  user_id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
+
+class Message(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  date = db.Column(db.DateTime(timezone=True), default=func.now())
+  text = db.Column(db.String(1000), nullable=False)
+  user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+  chat_id = db.Column(db.Integer, db.ForeignKey('chat.chat_id'))
 
 class Friend(db.Model):
     id = db.Column(db.Integer, primary_key=True)
