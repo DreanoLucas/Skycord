8aee66ca2f4cc7990254cb28dbe8bafb2e2340dc
Dreano Lucas <49568908+DreanoLucas@users.noreply.github.com>
2024-01-07 19:25:51 +0100

    resolution de probleme avec les groupes

--- website/views.py
+++ website/views.py
@@ -17,16 +17,18 @@ Dependencies:
     - current_user: Function from Flask-Login to get the current logged-in user.
 """
 
-from flask import Blueprint, render_template, url_for, redirect, session
+from flask import Blueprint, render_template, url_for, redirect
 from flask_login import login_required, current_user
 from . import db
 from .models import *
 views = Blueprint('views', __name__)
 
+from sqlalchemy import func, case
+from sqlalchemy.orm import aliased
+
 @views.route('/')
 @login_required
 def home():
-
     """Renders the home page.
 
     Checks if the user is logged in (using Flask-Login's login_required decorator).
@@ -36,26 +38,42 @@ def home():
         Rendered template: Renders the 'acceuil.html' template with user information.
     """
 
-    
-    subquery = db.session.query(Message.chat_id,
-                              func.max(
-                                  Message.date).label('max_date')).group_by(
-                                      Message.chat_id).subquery()
 
-    user_chats = db.session.query(
-      Chat.chat_id, Chat.chat_name, Message.text,
-      Message.date).join(ChatMember, ChatMember.chat_id == Chat.chat_id).join(
-            User, User.id == ChatMember.user_id).join( subquery, subquery.c.chat_id == Chat.chat_id, isouter=True).join(
-                    Message,
-                    db.and_(Message.chat_id == Chat.chat_id, Message.date == subquery.c.max_date), isouter=True).filter(
-                            User.id == current_user.id).order_by(Message.date.desc())
 
-    messages = session.pop('sorted_messages', None)
-    receiverName = ""
-    if messages:
-        receiverName = messages[0]['Receiver']
+    subquery = db.session.query(
+        Message.chat_id,
+        func.max(Message.date).label('max_date')
+    ).group_by(Message.chat_id).subquery()
+
+    max_date_message = aliased(Message, subquery)
+
+    user_chats = (
+        db.session.query(
+            Chat.chat_id,
+            Chat.chat_name,
+            Message.text,
+            Message.date
+        )
+        .join(ChatMember, ChatMember.chat_id == Chat.chat_id)
+        .join(User, User.id == ChatMember.user_id)
+        .join(subquery, subquery.c.chat_id == Chat.chat_id)
+        .outerjoin(
+            Message,
+            db.and_(
+                Message.chat_id == Chat.chat_id,
+                Message.date == subquery.c.max_date
+            )
+        )
+        .filter(User.id == current_user.id)
+        .order_by(
+            case((Message.date.is_(None), 0),
+                else_=Message.date
+            ).asc(),
+            Message.date.desc()
+        )
+    )
+    print(f'test {user_chats.all()}')
     return render_template('page_accueil.html',
                             user=current_user,
-                            username = receiverName,
-                            chats=user_chats.all(),
-                            messages=messages)
+                            chats=user_chats.all()
+                            )
